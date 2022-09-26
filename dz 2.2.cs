8d5b7fe92a39a89863c812ea9494383ed32f9596
DmitrinK <113488835+DmitrinK@users.noreply.github.com>
2022-09-27 00:29:13 +1000

    Add files via upload

---
+++ dz 2.2.cs
@@ -0,0 +1,15 @@
+﻿// программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("введите ваше число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber/100<1)
+{
+    Console.WriteLine("третьей цифры нет");
+    return;
+}
+int numS = userNumber;
+while (numS/100 >= 10)
+{
+    numS= numS/10;
+}
+int numShows = numS% 10;
+Console.WriteLine($"{numShows}");
---
+++ dz2.1.cs
@@ -0,0 +1,17 @@
+﻿// программа, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("введите трёхзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber/100 < 1)
+{
+    Console.WriteLine("ошибка, число не трёхзначное");
+    return;
+}
+if (userNumber/100 >= 10)
+{
+    Console.WriteLine("ошибка, число не трёхзначное");
+    return;
+}
+int numP = userNumber/10;
+int showN = numP% 10;
+Console.WriteLine($" {showN}");
